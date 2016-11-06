5c99ec35d42f88b9cdb6b992dfa5a9202e159934
Kezhong Zhang <zhangkz@bupt.edu.cn>
2016-11-06 23:30:34 +0800

    Read new data.

--- STING_DEMO.m
+++ STING_DEMO.m
@@ -2,9 +2,11 @@ clear;
 close all;
 % function STING_DEMO()
 % % Input the data
-% load('./Data/recover_128QAM.mat');
+load('./Data/recover_128QAM.mat');
 % load('./Data/recover_8PSK.mat');
-load('./Data/recover_16PSK.mat');
+% load('./Data/recover_16PSK.mat');
+% load('./Data/record_256QAM_-19dBm.mat');
+% load('./Data/recover_128QAM_-19dBm_2.mat');
 % load('./Data/recover_16QAM.mat');
 signal_recover=signal_recover/(max(max(abs(signal_recover))));
 DATA=[real(signal_recover),imag(signal_recover)];
@@ -15,7 +17,7 @@ Y = DATA(:,2);   % Y-coordinate
 GRID = [];       % grid division parameter. If choose[], is means that we estimate the length of square throuth uniform distribution.
 DENSITY =1;      % The threshold of points. Let a denotes the number of point locating in square A. If a<DENSITY, we do not select this square, otherwise, we select this square.
 NEIGHBOR = 8;    % Choose 4 or 8. The number of neighbourhood we test.
-MINPTS = 6;      % If the distance of two clusters is less than MINPTS, we combine these two clusters into a bigger one.
+MINPTS = 5;      % If the distance of two clusters is less than MINPTS, we combine these two clusters into a bigger one.
 
 % STING CLUSTERING
 [L,center,number_of_cluster]=STING(X,Y,GRID,DENSITY,NEIGHBOR,MINPTS,'Color','b','LineWidth',1,'LineStyle','-');
@@ -23,12 +25,27 @@ MINPTS = 6;      % If the distance of two clusters is less than MINPTS, we combi
 % [X,Y]=find(L~=0);
 % GRID = [];       % grid division parameter. If choose[], is means that we estimate the length of square throuth uniform distribution.
 % DENSITY =1;      % The threshold of points. Let a denotes the number of point locating in square A. If a<DENSITY, we do not select this square, otherwise, we select this square.
-% NEIGHBOR = 4;    % Choose 4 or 8. The number of neighbourhood we test.
-% MINPTS = 1;      % If the distance of two clusters is less than MINPTS, we combine these two clusters into a bigger one.
+% NEIGHBOR = 8;    % Choose 4 or 8. The number of neighbourhood we test.
+% MINPTS = 2;      % If the distance of two clusters is less than MINPTS, we combine these two clusters into a bigger one.
 % figure
 % [L,center,number_of_cluster]=STING(X,Y,GRID,DENSITY,NEIGHBOR,MINPTS,'Color','b','LineWidth',1,'LineStyle','-');
+% %---------------------------------------------------------------------------------------------------------------
+% % %---------------------------------------------------------------------------------------------------------------
+% [X,Y]=find(L~=0);
+% GRID = [];       % grid division parameter. If choose[], is means that we estimate the length of square throuth uniform distribution.
+% DENSITY =1;      % The threshold of points. Let a denotes the number of point locating in square A. If a<DENSITY, we do not select this square, otherwise, we select this square.
+% NEIGHBOR = 4;    % Choose 4 or 8. The number of neighbourhood we test.
+% MINPTS = 2;      % If the distance of two clusters is less than MINPTS, we combine these two clusters into a bigger one.
+% figure
+% [L,center,number_of_cluster]=STING(X,Y,GRID,DENSITY,NEIGHBOR,MINPTS,'Color','b','LineWidth',1,'LineStyle','-');
+% % %---------------------------------------------------------------------------------------------------------------
+center_complex=center*[1;1i];
+phase=mean(angle(center_complex));
+center_complex=center_complex*exp(-1i*phase);
+center=[real(center_complex),imag(center_complex)];
 % %---------------------------------------------------------------------------------------------------------------
 figure;scatter(center(:,1),center(:,2),'p');
 axis equal;grid on;
 % end % // STING demo
 %END OF PROGRA
+
